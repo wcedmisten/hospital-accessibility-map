2a4a44f0af3582963c5e85549834d5f3f0a493ea
William Edmisten <wcedmisten@gmail.com>
2023-04-15 16:07:07 -0400

    Skip alaska and PR

--- population.py
+++ population.py
@@ -14,6 +14,8 @@ from shapely.ops import transform
 import pyproj
 from pyproj import Geod
 
+import numpy as np
+
 import faulthandler
 
 faulthandler.enable()
@@ -45,13 +47,22 @@ project_to_mollweide = pyproj.Transformer.from_proj(
 def estimate_population(dataset, geom):
   polygon = shape(geom)
 
+  print("Projecting polygon to ESRI:54009")
   projected = transform(project_to_mollweide.transform, polygon)
 
   window = geometry_window(dataset, [projected], pad_x=10, pad_y=10)
 
-  data_window = dataset.read(masked=True, window=window)[0]
+  print(dataset.width, dataset.height)
+  print(window)
 
-  zs = zonal_stats(projected, data_window, affine=data_window.affine stats="sum")
+  print("Reading data window")
+  data_window = dataset.read(masked=False, window=window)[0]
+
+  affine = dataset.window_transform(window)
+
+  print("Calculating population statistics")
+  zs = zonal_stats(projected, data_window, stats="sum", affine=affine, nodata=np.nan)
+
   return round(zs[0]["sum"])
   
 # sanity check
@@ -73,26 +84,27 @@ for i in range(4):
 
 for state in states["features"]:
     state_name = state["properties"]["name"]
-    if state_name != "Hawaii":
+    if state_name == "Alaska" or state_name == "Puerto Rico":
         continue
 
     state_geom = shape(state["geometry"])
-
-    print(type(state_geom))
-    in_state = estimate_population(dataset, state_geom)
-
-    print(f"Estimated population of {state_name}: {in_state}")
 
     geod = Geod(ellps="WGS84")
     state_area = abs(geod.geometry_area_perimeter(state_geom)[0])
 
     print(f"Area of {state_name}: {state_area}")
 
+    print("Estimating state population")
+    in_state = estimate_population(dataset, state_geom)
+
+    print(f"Estimated population of {state_name}: {in_state}")
+
+
     times = [40, 30, 20, 10]
 
     state_data[state_name] = {}
 
-    for iso_geom in iso_geoms:
+    for iso_geom, time in zip(iso_geoms, times):
         # intersection is needed to cut off isochrones that extend beyond VA borders
         # this is because the OSM extract includes roads that don't
         # get cut strictly at the border, but extend into other states
@@ -103,13 +115,13 @@ for state in states["features"]:
 
         near_hospitals_area = abs(geod.geometry_area_perimeter(iso_va_intersection)[0])
 
-        print(f"Estimated {near_hospitals} residents within {times[i]} mins of a hospital.")
+        print(f"Estimated {near_hospitals} residents within {time} mins of a hospital.")
         print(f"{near_hospitals / in_state * 100} percent of {state_name} residents")
         print(f"Area of this region: {near_hospitals_area}")
         print(f"{near_hospitals_area / state_area * 100} percent of {state_name} surface area")
         print()
 
-        state_data[state_name][times[i]] = {
+        state_data[state_name][time] = {
            "population_absolute": near_hospitals,
            "state_population_absolute": in_state,
            "population_percent": near_hospitals / in_state * 100,
